3669371558e06b8397c87a7d6eb967b2cff93e16
antic <antic@DESKTOP-HTBO2JS>
2022-06-27 15:54:19 +0200

    Patients service and prescription service added

---
+++ src/components/Prescription/AddPrescription.jsx
@@ -0,0 +1,145 @@
+
+import { Form, Button } from "react-bootstrap"
+import { Component, useState } from 'react';
+import PrescriptionService from "../../services/PrescriptionService";
+import MultiSelect from 'multiselect-react-dropdown';
+
+class AddPrescription extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = { medicine: this.props.medicines, examination: this.props.examination, prescriptionId: 9999, selectedMedicines: [] };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.changeMedicines = this.changeMedicines.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onSelect = this.onSelect.bind(this);
+        this.onRemove = this.onRemove.bind(this);
+    }
+
+    changeMedicines(event) {
+        console.log("aaaaaaaaa");
+
+        let value2 = Array.from(
+            event.target.selectedOptions,
+            (option) => option.value
+        );
+
+        console.log(value2);
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        console.log(value);
+        this.setState({
+            medicine: value2
+        });
+    }
+
+
+    handleChange(event) {
+
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        console.log(target + " / tar" + value + "val / " + name);
+        this.setState({
+            [name]: value
+        });
+    }
+
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        console.log('prescribe => ' + JSON.stringify(this.state));
+
+        PrescriptionService.createPrescription({
+            "prescription": {
+                "examination": this.state.examination,
+                "prescriptionId": 9999,
+                "examinationId": this.state.examination.examinationId,
+                "disease": this.state.disease,
+                "description": this.state.description,
+
+            },
+            "medicines": this.state.selectedMedicines
+        })
+
+
+        // PrescriptionService.createPrescription({
+        //     "examination": this.state.examination,
+        //     "prescriptionId": 9999,
+        //     "examinationId": this.state.examination.examinationId,
+        //     "disease": this.state.disease,
+        //     "description": this.state.description,
+        //     "medicines": this.state.selectedMedicines
+        // });
+    }
+
+    onSelect(event) {
+        this.setState({
+            "selectedMedicines": event
+        });
+        console.log(event);
+        console.log(this.state.selectedMedicines);
+    }
+
+    onRemove(event) {
+        this.setState({
+            "selectedMedicines": event
+        });
+        console.log(event);
+        console.log(this.state.selectedMedicines);
+    }
+
+    render() {
+        return (
+            <>
+                {console.log(this.state.medicine)}
+                <Form onSubmit={this.handleSubmit}>
+                    <h5>Disease</h5>
+                    <Form.Group>
+                        <Form.Control
+                            type="text"
+                            placeholder="Disease *"
+                            name="disease"
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </Form.Group>
+
+                    <h5>Description</h5>
+                    <Form.Group>
+                        <Form.Control
+                            as="textarea"
+                            placeholder="Description *"
+                            name="description"
+                            rows={5}
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <h5>Medicines</h5>
+                        <MultiSelect
+                            options={this.state.medicine}
+                            displayValue="name"
+                            onSelect={this.onSelect}
+                            onRemove={this.onRemove}
+
+                        />
+                    </Form.Group>
+
+                    <Button variant="success" type="submit" block='true'>
+                        Add Presciption
+                    </Button>
+                </Form>
+
+            </>
+        )
+    }
+}
+
+export default AddPrescription;
---
+++ src/components/Prescription/ListPrescriptionsComponent.jsx
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import PrescriptionService from '../../services/PrescriptionService';
+import DetailsPrescription from '../Prescription/DetailsPrescription';
+
+function formatDate(date) {
+
+    return (new Date(Date.parse(date))).toLocaleString("en-GB", {
+        day: "numeric",
+        month: "short",
+        year: "numeric",
+        hour: "numeric",
+        minute: "2-digit"
+    });
+
+}
+
+class ListPrescriptionsComponent extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            prescriptions: [],
+            showDetails: false,
+            prescriptionDetails: [],
+            patientId: props.patientId,
+        }
+    }
+
+    async componentDidMount() {
+        await PrescriptionService.getPrescriptionsByPatient(this.state.patientId).then((res) => {
+            console.log("vratio prescriptions");
+
+            console.log(res);
+            this.setState({ prescriptions: res.data });
+        });
+    }
+
+    handleShowDetails = (prescription) => {
+        this.setState({
+            showDetails: true,
+            prescriptionDetails: prescription
+        });
+    }
+
+    handleCloseDetails = () => {
+        this.setState({
+            showDetails: false,
+            prescriptionDetails: []
+        });
+    }
+
+    render() {
+        return (
+            <div>
+
+                {
+                    this.state.prescriptions.length === 0
+                        ? <h2>There are no prescriptions</h2>
+                        : <div>
+                            <div className="row">
+
+                                <table className='table table-light table-striped table-bordered'>
+
+                                    <thead>
+                                        <tr>
+                                            <th>Doctor</th>
+                                            <th>Patient</th>
+                                            <th>Date</th>
+                                            <th>Details</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        {
+                                            this.state.prescriptions.map(
+                                                prescription => {
+                                                    return <>
+                                                        <tr key={prescription.examination.examinationId}>
+                                                            <td> {prescription.examination.doctor.firstName} {prescription.examination.doctor.lastName}</td>
+                                                            <td> {prescription.examination.patient.firstName} {prescription.examination.patient.lastName}</td>
+                                                            <td> {formatDate(prescription.examination.dateOfExamination)} </td>
+
+                                                            <td><Button onClick={() => this.handleShowDetails(prescription)} className='btn btn-info' data-toggle="modal">Details</Button> </td>
+                                                        </tr>
+                                                    </>
+                                                }
+                                            )
+                                        }
+                                    </tbody>
+
+                                </table>
+
+                            </div>
+                        </div>
+                }
+
+                {
+                    this.state.patientId != undefined
+                        ? <div className='add-examination'>
+                            <button variant="success" type="submit" block='true' className='btn btn-primary' onClick={this.handleShowCreate}>
+                                Appoint
+                            </button>
+                        </div>
+                        : <></>
+                }
+
+                <Modal show={this.state.showDetails} onHide={this.handleCloseDetails}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>
+                            Prescription details
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <DetailsPrescription prescription={this.state.prescriptionDetails} />
+                    </Modal.Body>
+                </Modal>
+
+            </div>
+        );
+    }
+}
+
+export default ListPrescriptionsComponent;
