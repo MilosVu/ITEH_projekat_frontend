f5b65efcfae566f38230df74fbb009445c3401dd
MilosVu <vujic.milos1998@gmail.com>
2022-06-27 15:14:55 +0200

    Doctor login kreiran

---
+++ src/components/Login/DoctorLogin.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import LoginFormComponent from './LoginFormComponent';
+
+class DoctorLogin extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {data: "doctor"};
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default DoctorLogin;
